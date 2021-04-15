bef4b8e059a661f2f7f91a8da2a3d9c4d534c109
Tuntenfisch <constantin.blessing.internet@gmail.com>
2021-04-15 23:51:47 +0200

    Removed deprecated comment.

--- Assets/Compute/CubicalMarchingSquares/Include/MarchingSquares.hlsl
+++ Assets/Compute/CubicalMarchingSquares/Include/MarchingSquares.hlsl
@@ -6,9 +6,6 @@
 // 3         1
 // |         |
 // +----0----+
-//
-// The order of the segments for each case matters and is taken
-// from https://gist.github.com/TheCyberBrick/f798c0d79cf207cdbbf3033332e9a909.
 static const uint4 marchingSquaresSegments[16] =
 {
     uint4(-1, -1, -1, -1),
