6fb7606a98145554df014643186489e7991dfe05
Tuntenfisch <constantin.blessing.internet@gmail.com>
2021-04-21 12:44:50 +0200

    Work.

---
+++ Assets/Scripts/Voxel/ComputeShaderProperties.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Voxel
+{
+    public static class ComputeShaderProperties
+    {
+        public static readonly int s_cellDimensions = Shader.PropertyToID("cellDimensions");
+        public static readonly int s_cellSpacing = Shader.PropertyToID("cellSpacing");
+        public static readonly int s_cellVolumeToWorldSpaceOffset = Shader.PropertyToID("cellVolumeToWorldSpaceOffset");
+        public static readonly int s_cosOfSharpFeatureAngle = Shader.PropertyToID("cosOfSharpFeatureAngle");
+        public static readonly int s_flatFeatureVertexIndicesLookupTable = Shader.PropertyToID("flatFeatureVertexIndicesLookupTable");
+        public static readonly int s_generatedTriangles = Shader.PropertyToID("generatedTriangles");
+        public static readonly int s_generatedVertices = Shader.PropertyToID("generatedVertices");
+        public static readonly int s_height = Shader.PropertyToID("height");
+        public static readonly int s_lacunarity = Shader.PropertyToID("lacunarity");
+        public static readonly int s_maxIterations = Shader.PropertyToID("maxIterations");
+        public static readonly int s_numberOfOctaves = Shader.PropertyToID("numberOfOctaves");
+        public static readonly int s_octaveOffsets = Shader.PropertyToID("octaveOffsets");
+        public static readonly int s_persistence = Shader.PropertyToID("persistence");
+        public static readonly int s_stepSize = Shader.PropertyToID("stepSize");
+        public static readonly int s_subSampledCellVolumeFaces = Shader.PropertyToID("subSampledCellVolumeFaces");
+        public static readonly int s_voxelDimensions = Shader.PropertyToID("voxelDimensions");
+        public static readonly int s_voxelVolume = Shader.PropertyToID("voxelVolume");
+        public static readonly int s_wavelength = Shader.PropertyToID("wavelength");
+    }
+}
---
+++ Assets/Scripts/Voxel/Vertex.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace Voxel
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct Vertex
+    {
+        public static VertexAttributeDescriptor[] Attributes => s_attributes;
+
+        public static int SizeInBytes => s_sizeInBytes;
+
+        private static readonly VertexAttributeDescriptor[] s_attributes =
+        {
+            new VertexAttributeDescriptor(VertexAttribute.Position, VertexAttributeFormat.Float32, 3),
+            new VertexAttributeDescriptor(VertexAttribute.Normal, VertexAttributeFormat.Float32, 3)
+        };
+
+        private static readonly int s_sizeInBytes = 6 * sizeof(float);
+
+        private Vector3 m_position;
+        private Vector3 m_normal;
+    }
+}
