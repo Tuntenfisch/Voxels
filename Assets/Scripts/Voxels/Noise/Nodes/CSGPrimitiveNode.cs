89b908d3b1f848b398ee978d7680ce98c0a63c9c
Tuntenfisch <constantin.blessing.internet@gmail.com>
2021-06-25 09:26:26 +0200

    Renamed some noise graph node inputs/outputs.

--- Assets/Scripts/Voxels/Noise/Nodes/CSGPrimitiveNode.cs
+++ Assets/Scripts/Voxels/Noise/Nodes/CSGPrimitiveNode.cs
@@ -16,7 +16,7 @@ namespace Tuntenfisch.Voxels.Noise.Nodes
 
         [Output(backingValue = ShowBackingValue.Never, connectionType = ConnectionType.Override, typeConstraint = TypeConstraint.Strict)]
         [SerializeField]
-        private float4 m_valueAndGradient;
+        private float4 m_voxel;
 
         [SerializeField]
         private GPUCSGPrimitive m_csgPrimitive;
