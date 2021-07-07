275f52c237c0f0dc3d8c2f00f0387296f0028024
Tuntenfisch <constantin.blessing.internet@gmail.com>
2021-07-07 20:58:55 +0200

    New textures.

--- Assets/Shaders/Voxels/Include/Triplanar.hlsl
+++ Assets/Shaders/Voxels/Include/Triplanar.hlsl
@@ -3,38 +3,29 @@
 
 #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
 
-void GetTriplanarProjections(float3 position, half3 normal, out float2 xProjection, out float2 yProjection, out float2 zProjection)
+void GetTriplanarProjections(float3 positionWS, half3 normalWS, out float2 uvX, out float2 uvY, out float2 uvZ)
 {
-    position *= _TriplanarCoordinateScaling;
+    positionWS *= _TriplanarCoordinateScaling;
 
-    xProjection = position.zy;
-    yProjection = position.xz;
-    zProjection = position.xy;
+    uvX = positionWS.zy;
+    uvY = positionWS.xz;
+    uvZ = positionWS.xy;
+
+    half3 signs = sign(normalWS);
 
     // Account for mirrored mapping.
-    if (normal.x < 0.0h)
-    {
-        xProjection.x = -xProjection.x;
-    }
-
-    if (normal.y < 0.0h)
-    {
-        yProjection.x = -yProjection.x;
-    }
-
-    if (normal.z >= 0.0h)
-    {
-        zProjection.x = -zProjection.x;
-    }
+    uvX.x *= signs.x;
+    uvY.x *= signs.y;
+    uvZ.x *= -signs.z;
 
     // Offset coordinates to prevent sudden repetitions.
-    xProjection.y += 0.5h;
-    zProjection.x += 0.5h;
+    uvX.y += 0.5h;
+    uvZ.x += 0.5h;
 }
 
-float3 GetTriplanarWeights(float3 normal)
+float3 GetTriplanarWeights(float3 normalWS)
 {
-    float3 triplanarWeights = abs(normal);
+    float3 triplanarWeights = abs(normalWS);
     triplanarWeights = saturate(triplanarWeights - _TriplanarBlendOffset);
     triplanarWeights = pow(triplanarWeights, _TriplanarBlendExponent);
     triplanarWeights /= dot(triplanarWeights, 1.0f);
@@ -42,16 +33,16 @@ float3 GetTriplanarWeights(float3 normal)
     return triplanarWeights;
 }
 
-half4 TriplanarSampleTexture2DArray(float3 position, half3 normal, TEXTURE2D_ARRAY(textureName), SAMPLER(samplerName), uint index)
+half4 TriplanarSampleTexture2DArray(float3 positionWS, half3 normalWS, TEXTURE2D_ARRAY(textureName), SAMPLER(samplerName), uint index)
 {
-    float2 xProjection, yProjection, zProjection;
-    GetTriplanarProjections(position, normal, xProjection, yProjection, zProjection);
+    float2 uvX, uvY, uvZ;
+    GetTriplanarProjections(positionWS, normalWS, uvX, uvY, uvZ);
 
-    float3 triplanarWeights = GetTriplanarWeights(normal);
+    float3 triplanarWeights = GetTriplanarWeights(normalWS);
 
-    half4 valueX = SAMPLE_TEXTURE2D_ARRAY(textureName, samplerName, xProjection, index);
-    half4 valueY = SAMPLE_TEXTURE2D_ARRAY(textureName, samplerName, yProjection, index);
-    half4 valueZ = SAMPLE_TEXTURE2D_ARRAY(textureName, samplerName, zProjection, index);
+    half4 valueX = SAMPLE_TEXTURE2D_ARRAY(textureName, samplerName, uvX, index);
+    half4 valueY = SAMPLE_TEXTURE2D_ARRAY(textureName, samplerName, uvY, index);
+    half4 valueZ = SAMPLE_TEXTURE2D_ARRAY(textureName, samplerName, uvZ, index);
 
     return triplanarWeights.x * valueX + triplanarWeights.y * valueY + triplanarWeights.z * valueZ;
 }
