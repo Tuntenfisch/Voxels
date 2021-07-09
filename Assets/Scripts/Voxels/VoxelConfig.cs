1eeb00c3ee78296ff5693bdd081152b0761271f0
Tuntenfisch <constantin.blessing.internet@gmail.com>
2021-07-09 18:38:04 +0200

    VoxelConfig now applies the individual configs when they change.

--- Assets/Scripts/Voxels/VoxelConfig.cs
+++ Assets/Scripts/Voxels/VoxelConfig.cs
@@ -2,6 +2,7 @@
 using Tuntenfisch.Voxels.Materials;
 using Tuntenfisch.Voxels.Procedural;
 using Tuntenfisch.Voxels.Volume;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.Assertions;
 
@@ -29,6 +30,37 @@ namespace Tuntenfisch.Voxels
             Assert.IsNotNull(m_materialConfig);
             Assert.IsNotNull(m_dualContouringConfig);
             Assert.IsNotNull(m_generationGraph);
+
+            VoxelVolumeConfig.OnDirtied += ApplyVoxelVolumeConfig;
+            DualContouringConfig.OnDirtied += ApplyDualContouringConfig;
+
+            ApplyVoxelVolumeConfig();
+            ApplyDualContouringConfig();
+        }
+
+        private void OnDestroy()
+        {
+            VoxelVolumeConfig.OnDirtied -= ApplyVoxelVolumeConfig;
+            DualContouringConfig.OnDirtied -= ApplyDualContouringConfig;
+        }
+
+        private void ApplyVoxelVolumeConfig()
+        {
+            VoxelVolumeConfig.Compute.SetInts(ComputeShaderProperties.NumberOfVoxels, VoxelVolumeConfig.NumberOfVoxels.x, VoxelVolumeConfig.NumberOfVoxels.y, VoxelVolumeConfig.NumberOfVoxels.z);
+            VoxelVolumeConfig.Compute.SetFloat(ComputeShaderProperties.VoxelSpacing, VoxelVolumeConfig.VoxelSpacing);
+
+            int3 numberOfVoxels = VoxelVolumeConfig.NumberOfVoxels;
+            DualContouringConfig.Compute.SetInts(ComputeShaderProperties.NumberOfVoxels, numberOfVoxels.x, numberOfVoxels.y, numberOfVoxels.z);
+            DualContouringConfig.Compute.SetFloat(ComputeShaderProperties.VoxelSpacing, VoxelVolumeConfig.VoxelSpacing);
+        }
+
+        private void ApplyDualContouringConfig()
+        {
+            float cosOfHalfSharpFeatureAngle = math.cos(math.radians(0.5f * DualContouringConfig.SharpFeatureAngle));
+            Shader.SetGlobalFloat(nameof(cosOfHalfSharpFeatureAngle), cosOfHalfSharpFeatureAngle);
+
+            DualContouringConfig.Compute.SetInt(ComputeShaderProperties.SchmitzParticleIterations, DualContouringConfig.SchmitzParticleIterations);
+            DualContouringConfig.Compute.SetFloat(ComputeShaderProperties.SchmitzParticleStepSize, DualContouringConfig.SchmitzParticleStepSize);
         }
     }
 }
